62157c03b6ef4c313adc39695a9ad04540ab8f10
cicomsa <ioanacomsa85@gmail.com>
2020-07-03 16:38:17 +0100

    exerciseTwelve - removed square brackets

--- exercises/exerciseTwelve.js
+++ exercises/exerciseTwelve.js
@@ -1,4 +1,6 @@
 const logic = array =>
-  array.reduce((flattenedList, i) => flattenedList.concat(Array.isArray(i) ? logic(i) : [i]), [])
+  array.reduce((flattenedList, i) =>
+    flattenedList.concat(Array.isArray(i) ? logic(i) : i)
+    , [])
 
 module.exports = logic
